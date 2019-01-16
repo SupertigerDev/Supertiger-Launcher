5cf95afea4d0461ebc7c0f2054dfd9a1a63cdb53
CZHO404 <CZHO404@github.com>
2019-01-16 12:01:09 -0700

    asd

--- src/utils/index.js
+++
@@ -1,8 +0,0 @@
-function loadTheme() {
-    theme = store.get('theme')
-    if (theme == undefined) {
-        themeLink.removeAttribute('href')
-        return;
-    }
-    themeLink.setAttribute('href', "../../themes/" + theme.folderName+"/" +theme.config.fileName )
-}
